f4b0cc2505d943e529c9d7e91d4d7fdc9a68dc9c
Zekiloni <zekirija17@gmail.com>
2023-05-20 17:49:11 +0200

    StringUtilsMixin

--- src/utils/stringUtils.ts
+++ src/utils/stringUtils.ts
@@ -1,32 +1,29 @@
 
-import { App } from 'vue';
+import { Vue } from "vue-class-component";
 
-const stringUtils = {
-    methods: {
-        concat(...values: string[]): string {
-            return values.join('');
-        },
 
-        truncate(value: string, maxLength: number): string {
-            return maxLength && value.length > maxLength ? value.slice(0, maxLength - 3) + '...' : value;
-        },
-
-        capitalize(value: string): string {
-            return value.charAt(0).toUpperCase() + value.slice(1);
-        },
-
-        formatCurrency(value: number, currencySymbol: string = '$'): string {
-            return currencySymbol + value.toFixed(2);
-        },
-
-        reverse(value: string): string {
-            return value.split('').reverse().join('');
-        },
-
-        isEmpty(value: string): boolean {
-            return value.trim().length === 0;
-        }
+export default class StringUtils extends Vue {
+    concat(...values: string[]): string {
+        return values.join('');
     }
-};
 
-export default stringUtils;
+    truncate(value: string, maxLength: number): string {
+        return maxLength && value.length > maxLength ? value.slice(0, maxLength - 3) + '...' : value;
+    }
+
+    capitalize(value: string): string {
+        return value.charAt(0).toUpperCase() + value.slice(1);
+    }
+
+    formatCurrency(value: number, currencySymbol: string = '$'): string {
+        return currencySymbol + value.toFixed(2);
+    }
+
+    reverse(value: string): string {
+        return value.split('').reverse().join('');
+    }
+
+    isEmpty(value: string): boolean {
+        return value.trim().length === 0;
+    }
+}
