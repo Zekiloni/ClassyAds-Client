173d42889cefcb0c5b7d962ee2bda205fd097b4e
Zekiloni <zekirija17@gmail.com>
2023-06-15 01:37:19 +0200

    webSocketService

---
+++ src/services/webSocketService.ts
@@ -0,0 +1,49 @@
+import { client as WebSocketClient, connection as WebSocketConnection } from "websocket";
+
+export default class WebSocketService {
+    public client: WebSocketClient | null;
+    public connection: WebSocketConnection | null;
+
+    constructor() {
+        this.client = new WebSocketClient();
+        this.connection = null;
+    }
+
+    connect(url: string) {
+        if (!this.client) {
+            return;
+        }
+
+        this.client.on('connect', (connection) => {
+            this.connection = connection;
+
+            console.log('WebSocket connection opened.');
+
+            connection.on('message', (message) => {
+                if (message.type === 'utf8') {
+                    const data = JSON.parse(message.utf8Data);
+                }
+            });
+
+            connection.on('close', () => {
+                console.log('WebSocket connection closed.');
+            });
+        });
+
+        this.client.connect(url);
+    }
+
+    send(message: string) {
+        if (this.connection && this.connection.connected) {
+            this.connection.sendUTF(JSON.stringify(message));
+        }
+    }
+
+    disconnect() {
+        if (this.connection && this.connection.connected) {
+            this.connection.close();
+            this.connection = null;
+        }
+    }
+}
+
