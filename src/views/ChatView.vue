e4901fc30c6ebf8dcde8e88dde31bffebfac908c
Zekiloni <zekirija17@gmail.com>
2023-06-15 01:37:41 +0200

    ChatView page & web socket connection

---
+++ src/views/ChatView.vue
@@ -0,0 +1,23 @@
+<template></template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+
+import WebSocketService from "@/services/webSocketService";
+
+
+export default class ChatView extends Vue {
+    socket: WebSocketService | null = null;
+
+    mounted() {
+        this.socket = new WebSocketService();
+        this.socket.connect(process.env.VUE_APP_SOCKET_URL!);
+    }
+
+    beforeDestroy() {
+        if (this.socket) {
+            this.socket.disconnect();
+        }
+    }
+}
+</script>
